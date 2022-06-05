6dcfff994bd008b832f5a6c3a6dee19d3cc171f7
Bohdan <night.prowler112@gmail.com>
2022-06-05 17:00:32 +0300

    possibility of switching between english and ukrainian languages has been provided

--- pomodoro/pomodoro.js
+++ pomodoro/pomodoro.js
@@ -1,42 +1,35 @@
 class Pomodoro {
-  constructor(focusSession, shortBreak, longBreak) {
+  constructor(focusSession, shortBreak, longBreak, textContent) {
     this.focusSession = focusSession * 60;
     this.shortBreak = shortBreak * 60;
     this.longBreak = longBreak * 60;
+    this.textContent = textContent;
+
+    this.ding = document.getElementById("ding");
 
     this.pomodorosCounter = 0;
-    this.nextMode = "focus";
-
-    this.timeout;
-
-    this.display = document.getElementById("time");
-    this.ding = document.getElementById("ding");
-    this.title = document.getElementById("title");
     this.counter = document.getElementById("counter");
+    this.counter.textContent = this.pomodorosCounter;
+    this.display = document.getElementById("time");
     this.display.textContent = this.getTimeString(this.focusSession);
 
+    this.nextMode = "focus";
+    this.timeout;
     this.currentTimer;
-
     this.remaining = this.focusSession;
-
     this.breakDuration = this.shortBreak;
   }
-
   getTimeString(time) {
     let min = Math.trunc(time / 60);
     let sec = time % 60;
-
     min = min < 10 ? "0" + min : "" + min;
     sec = sec < 10 ? "0" + sec : "" + sec;
-
     return `${min}:${sec}`;
   }
-
   countOneSecond(time) {
     this.timeout = setInterval(() => {
       this.remaining = --time;
       this.display.textContent = this.getTimeString(time);
-      document.title = this.getTimeString(time);
 
       if (time < 0) {
         this.ding.play();
@@ -44,63 +37,46 @@ class Pomodoro {
       }
     }, 1000);
   }
-
   startFocusSession() {
     document.body.style.background = "tomato";
-    this.title.textContent = "Focus";
 
     this.currentTimer = this.focusSession;
     this.pomodorosCounter++;
     this.currentMode = this.nextMode;
     this.nextMode = this.pomodorosCounter % 4 ? "shortBreak " : "longBreak";
-
     clearTimeout(this.timeout);
     this.display.textContent = this.getTimeString(this.focusSession);
     this.countOneSecond(this.focusSession);
-
     this.breakDuration =
       this.pomodorosCounter % 4 ? this.shortBreak : this.longBreak;
-
     this.remaining = this.focusSession;
   }
-
   startBreak() {
     document.body.style.background = "green";
-    this.title.textContent = "Rest";
-    this.counter.textContent = this.pomodorosCounter;
 
+    this.counter.textContent = this.pomodorosCounter;
     this.currentTimer =
       this.pomodorosCounter % 4 ? this.shortBreak : this.longBreak;
-
     this.currentMode = this.nextMode;
     this.nextMode = "focus";
-
     this.display.textContent = this.getTimeString(this.breakDuration);
     clearTimeout(this.timeout);
     this.countOneSecond(this.breakDuration);
-
     this.remaining = this.breakDuration;
   }
-
   start() {
     this.nextMode === "focus" ? this.startFocusSession() : this.startBreak();
-
     console.log(this.nextMode);
   }
-
   pause() {
     clearTimeout(this.timeout);
   }
-
   continue() {
     clearTimeout(this.timeout);
     this.countOneSecond(this.remaining);
   }
-
   stop() {
-    this.title.textContent = "Welcome!";
     document.body.style.background = "tomato";
-
     clearTimeout(this.timeout);
     this.nextMode = "focus";
     this.pomodorosCounter = 0;
@@ -108,18 +84,14 @@ class Pomodoro {
     this.remaining = this.focusSession;
     this.display.textContent = this.getTimeString(this.focusSession);
   }
-
   reset() {
     this.nextMode = this.currentMode;
-
     if (this.currentMode === "focus") {
       this.pomodorosCounter--;
     }
-
     clearTimeout(this.timeout);
     this.remaining = this.currentTimer;
     this.display.textContent = this.getTimeString(this.currentTimer);
-
     console.log(this.currentMode, this.pomodorosCounter);
   }
 }
--- pomodoro/script.js
+++ pomodoro/script.js
@@ -1,7 +1,16 @@
+// // TO DO NEXT
+
+// // Theme settings
+
 let focusSession = 25;
 let shortBreak = 5;
 let longBreak = 15;
-let pomodoro = new Pomodoro(focusSession, shortBreak, longBreak);
+let textContent = languages.english;
+
+let pomodoro = new Pomodoro(focusSession, shortBreak, longBreak, textContent);
+
+const title = document.getElementById("title");
+const pomodoros = document.getElementById("pomodoros");
 
 const buttons = document.querySelectorAll(".button");
 const startButton = document.getElementById("start");
@@ -13,9 +22,41 @@ const resetButton = document.getElementById("reset");
 const settingsButton = document.getElementById("settings");
 const settingsForm = document.getElementById("settings-form");
 const overlay = document.getElementById("overlay");
+
+const settingsTitle = document.getElementById("settings-title");
+const timerSettingsTitle = document.getElementById("timer-settings");
+
+const focusDuration = document.getElementById("focus-duration");
+const shortBreakDuration = document.getElementById("short-break-duration");
+const longBreakDuration = document.getElementById("long-break-duration");
+
 const focusInput = document.getElementById("focus");
 const shortBreakInput = document.getElementById("short-break");
 const longBreakInput = document.getElementById("long-break");
+
+const language = document.getElementById("language");
+const languageSelect = document.getElementById("language-select");
+
+const saveButton = document.getElementById("save");
+const cancelButton = document.getElementById("cancel");
+
+function setLanguage(textContent) {
+  pomodoros.textContent = textContent.pomodoros;
+  startButton.textContent = textContent.startButton;
+  pauseButton.textContent = textContent.pauseButton;
+  stopButton.textContent = textContent.stopButton;
+  continueButton.textContent = textContent.continueButton;
+  resetButton.textContent = textContent.resetButton;
+  settingsTitle.textContent = textContent.settingsTitle;
+  timerSettingsTitle.textContent = textContent.timerSettingsTitle;
+  focusDuration.textContent = textContent.focusDuration;
+  shortBreakDuration.textContent = textContent.shortBreakDuration;
+  longBreakDuration.textContent = textContent.longBreakDuration;
+  language.textContent = textContent.language;
+  saveButton.textContent = textContent.saveButton;
+  cancelButton.textContent = textContent.cancelButton;
+}
+setLanguage(textContent);
 
 function displayRemoveButtons(displayButtons, removeButtons) {
   displayButtons.forEach((button) => button.classList.remove("hidden"));
@@ -61,25 +102,58 @@ settingsButton.addEventListener("click", () => {
   overlay.style.zIndex = "2";
   settingsForm.style.zIndex = "3";
   settingsForm.style.opacity = "100%";
+  settingsForm.style.display = "flex";
 });
 
-settingsForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  pomodoro.stop();
-  displayRemoveButtons(
-    [startButton],
-    [stopButton, pauseButton, continueButton, resetButton]
-  );
-
-  focusSession = +focusInput.value;
-  shortBreak = +shortBreakInput.value;
-  longBreak = +longBreakInput.value;
-
+function closeSettings() {
   overlay.style.opacity = "0%";
   overlay.style.zIndex = "-1";
   settingsForm.style.zIndex = "-1";
   settingsForm.style.opacity = "0%";
+  settingsForm.style.display = "none";
+}
 
-  pomodoro = new Pomodoro(focusSession, shortBreak, longBreak);
+settingsForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  newFocusSession = +focusInput.value;
+  newShortBreak = +shortBreakInput.value;
+  newLongBreak = +longBreakInput.value;
+
+  console.log(focusSession, newFocusSession);
+  console.log(shortBreak, newShortBreak);
+  console.log(longBreak, newLongBreak);
+
+  if (
+    newFocusSession !== focusSession ||
+    newShortBreak !== shortBreak ||
+    newLongBreak !== longBreak
+  ) {
+    pomodoro.stop();
+    displayRemoveButtons(
+      [startButton],
+      [stopButton, pauseButton, continueButton, resetButton]
+    );
+
+    focusSession = newFocusSession;
+    shortBreak = newShortBreak;
+    longBreak = newLongBreak;
+
+    pomodoro = new Pomodoro(focusSession, shortBreak, longBreak, textContent);
+  }
+
+  textContent = languages[languageSelect.value];
+  pomodoro.textContent = textContent;
+
+  closeSettings();
+
+  setLanguage(textContent);
+});
+
+cancelButton.addEventListener("click", closeSettings);
+overlay.addEventListener("click", closeSettings);
+document.addEventListener("keyup", (e) => {
+  if (e.key === "Escape") {
+    closeSettings();
+  }
 });
